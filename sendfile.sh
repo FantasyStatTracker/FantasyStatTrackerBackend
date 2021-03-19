845d3d6eb11afd603f584e08034ceef55b8e5bb5
ajaypatel24 <ajay.patel@mail.mcgill.ca>
2021-03-19 00:44:43 -0400

    changes

--- sendfile.sh
+++ sendfile.sh
@@ -7,8 +7,11 @@ git push
 git checkout deploymentCheese
 git pull
 git checkout development output.json
+wait
 git add .
 git commit -m "update output.json" output.json
+wait
 git push
+wait
 
 git checkout development
