8b8016ddba211cb5b5e45936500a9df8da75fe31
ajaypatel24 <ajay.patel@mail.mcgill.ca>
2023-12-21 23:48:49 -0500

    changes

--- Variables/TokenRefresh.py
+++ Variables/TokenRefresh.py
@@ -1,17 +1,26 @@
 from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
 import os
+from pymongo import MongoClient
 
 global oauth
 global gm
 global lg
 global api_key
+global db
 
 
 oauth = OAuth2(None, None, from_file="oauth2.json")
 gm = yfa.Game(oauth, "nba")
+
 lg = gm.to_league(os.getenv("LEAGUE_ID"))  # 2022-23
 api_key = os.getenv("API_KEY")
+connection_string = "mongodb+srv://fantasy_user:mShmjJcog7iPBoVa@cluster29877.rvsjvxp.mongodb.net/?appName=mongosh+2.0.0".format(
+    credentials=os.getenv("MONGO_PASS"), username=os.getenv("MONGO_USER")
+)
+print(connection_string)
+client = MongoClient(connection_string)
+db = client.sample_training
 
 if not oauth.token_is_valid():
     oauth.refresh_access_token()
--- routes/NewApi.py
+++ routes/NewApi.py
@@ -1,12 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, Response, make_response
 from flask_cors import cross_origin
+from Variables.TokenRefresh import db
+from bson import json_util, decode_all
 
 Api = Blueprint("Api", __name__)
-
 # todo: Needs a lot of work
 
 
 @Api.route("/streak", methods=["GET"])  # winning
 @cross_origin()
 def update_roster_stats():
-    return ""
+    data = db.companies.find()
+    print(data)
+    res = []
+    # define data model for mongo outside of this and use that to make responses
+    for field in data:
+        g = {
+            "_id": str(field["_id"]),
+            "name": field["name"],
+            "permalink": field["permalink"],
+            "homepage_url": field["homepage_url"],
+        }
+        res.append(g)
+
+    return jsonify(res)
+
